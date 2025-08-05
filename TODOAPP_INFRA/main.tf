a543a9b56a0f8aad18867f34b5fc84b771c042e8
HarvirSingh <harveer25@gmail.com>
2025-08-05 18:23:39 +0530

    Added one more RG

--- TODOAPP_INFRA/main.tf
+++ TODOAPP_INFRA/main.tf
@@ -4,3 +4,9 @@ module "resource_group" {
   resource_group_name     = "rg-htodoapp"
 }
 
+module "resource_group" {
+  source                  = "../MODULES/azurerm_resource_group"
+  resource_group_location = "centralindia"
+  resource_group_name     = "rg-htodoapp1"
+}
+
