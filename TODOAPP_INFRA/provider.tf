f52d2b53dc43c9036ee58199db7c5c295e52b034
HarvirSingh <harveer25@gmail.com>
2025-08-24 23:21:21 +0530

    added backend

--- TODOAPP_INFRA/provider.tf
+++ TODOAPP_INFRA/provider.tf
@@ -5,9 +5,15 @@ terraform {
       version = "4.33.0"
     }
   }
+backend "azurerm" {
+  resource_group_name  = "HAR-RG"
+  storage_account_name = "harpracticestorage"
+  container_name       = "statecontainer"
+  key                  = "terraform.tfstate"
+}  
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "128e0202-3a1c-42af-a798-11f9e9c0422e"
+  subscription_id = "81cc1915-8d88-419e-8fa3-0178811761bd"
 }
