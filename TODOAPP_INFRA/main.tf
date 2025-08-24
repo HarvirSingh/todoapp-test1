bbf569a6a0ce3da0dc115b436abcb1bbf5ae4078
HarvirSingh <harveer25@gmail.com>
2025-08-24 23:51:41 +0530

    RG4 added

--- TODOAPP_INFRA/main.tf
+++ TODOAPP_INFRA/main.tf
@@ -16,9 +16,14 @@ module "resource_group2" {
   resource_group_name     = "rg-htodoapp2"
 }
 
-module "resource_group2" {
+module "resource_group3" {
   source                  = "../MODULES/azurerm_resource_group"
   resource_group_location = "eastus"
   resource_group_name     = "rg-htodoapp3"
 }
 
+module "resource_group4" {
+  source                  = "../MODULES/azurerm_resource_group"
+  resource_group_location = "eastus"
+  resource_group_name     = "rg-htodoapp3"
+}
--- TODOAPP_INFRA/provider.tf
+++ TODOAPP_INFRA/provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "4.33.0"
     }
   }
-backend "azurerm" {
-  resource_group_name  = "HAR-RG"
-  storage_account_name = "harpracticestorage"
-  container_name       = "statecontainer"
-  key                  = "terraform.tfstate"
-}  
+  backend "azurerm" {
+    resource_group_name  = "HAR-RG"
+    storage_account_name = "harpracticestorage"
+    container_name       = "statecontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
